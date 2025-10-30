1b16fdb0a320800125798b0418738c155e706be4
DerikMaiaa <derikemaia@gmail.com>
2025-10-30 09:03:11 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,21 @@
-//declaração de bibliotecas
-const express = require("express"); //colocou uma biblioteca dentro da variavel
-const { Client } = require("pg"); //puxou somente uma parte da biblioteca
+//Declarações das bibliotecas
+const express = require("express");
+const { Client } = require("pg");
 const cors = require("cors");
 const bodyparser = require("body-parser");
-const config=require('.config')
+const config=require('./config')
 
-
-//inicialização de variavel
+//Inicialização das variáveis
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyparser.json());
 
-//definição de conexão
+//Definição da conexão
 const connectionString = config.DATABASE_URL
 const client = new Client(connectionString);
 
-//rota para chamar apenas 1 usuario
-app.get("/usuarios/:id", (req, res) => {
-    try {
-        console.log("Rota: usuarios/" + req.params.id);
-        client.query
-            (
-                "SELECT * FROM Usuarios WHERE id = $1"
-                ,
-                [req.params.id
-                ]
-                ,
-
-                (err, result) => {
-                    if
-                        (err) {
-                        res.status
-                            (501).send
-                            ("Erro ao executar a qry" + err);
-                        return console.error
-                            ("Erro ao executar a qry de SELECT", err);
-
-                    }
-                    res.status
-                        (200).send
-                        (result.rows
-                        [
-                            0]);
-                    //console.log
-                    (result);
-
-                }
-            );
-    } catch (error) {
-        console.log
-            (error);
-
-    }
-});
-
-
-//primeira conexão [teste]
+//Primeira conexão(teste)
 client.connect((err) => {
     if (err) {
         return console.error('Não foi possível conectar ao banco.', err);
@@ -69,55 +28,13 @@ client.connect((err) => {
     });
 });
 
-//rota para excluir usuarios
-app.delete("/usuarios/:id", (req, res) => {
-    try {
-        console.log("Rota: delete/" + req.params.id);
-        client.query(
-            "DELETE FROM Usuarios WHERE id = $1", [req.params.id], (err, result) => {
-                if (err) {
-                    res.status(501).send("Erro ao executar a qry " + err);
-                    return console.error("Erro ao executar a qry de DELETE", err);
-                } else {
-                    if (result.rowCount == 0) {
-                        res.status(404).json({ info: "Registro não encontrado." });
-                    } else {
-                        res.status(200).json({ info: `Registro excluído. Código: ${req.params.id}` });
-                    }
-                }
-                console.log(result);
-            }
-        );
-    } catch (error) {
-        console.log(error);
-    }
+//Definição da primeira ROTA ou EndPoint da API
+app.get("/", (req, res) => {
+    console.log("Response ok.");
+    res.status(200).send("Ok – Servidor disponível.");
 });
 
-//rota para inserir usuarios
-app.post("/usuarios", (req, res) => {
-    try {
-        console.log("Alguém enviou um post com os dados:", req.body);
-        const { nome, email, altura, peso } = req.body;
-        client.query(
-            "INSERT INTO Usuarios (nome, email, altura, peso) VALUES ($1, $2, $3, $4) RETURNING * ", [nome, email, altura, peso],
-            (err, result) => {
-                if (err) {
-                    res.status(501).send("Erro ao executar a qry " + err);
-                    return console.error("Erro ao executar a qry de INSERT", err);
-                }
-                const { id } = result.rows[0];
-                res.setHeader("id", `${id}`);
-                res.status(201).json(result.rows[0]);
-                console.log(result);
-            }
-        );
-    } catch (erro) {
-        console.error(erro);
-    }
-});
-
-
-//rota para retornar todos os usuarios
+//Rota para retornar todos os usuarios
 app.get("/usuarios", (req, res) => {
     try {
         client.query("SELECT * FROM Usuarios", function (err, result) {
@@ -133,52 +50,94 @@ app.get("/usuarios", (req, res) => {
     }
 });
 
+//Rota para retornar um usuario especifico
+app.get("/usuarios/:id", (req, res) => {
+    try {
+        console.log("Rota: usuarios/" + req.params.id);
+        client.query("SELECT * FROM Usuarios WHERE id = $1", [req.params.id], (err, result) => {
+            if (err) {
+                res.status(501).send("Erro ao executar a qry" + err);
+                return console.error("Erro ao executar a qry de SELECT", err);
+            }
+            res.status(200).send(result.rows[0]);
+            //console.log(result);
+        }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+});
 
-//rota para atualizar um user especifico
+//Rota para excluir um usuario especifico
+app.delete("/usuarios/:id", (req, res) => {
+    try {
+        console.log("Rota: delete/" + req.params.id);
+        client.query("DELETE FROM Usuarios WHERE id = $1", [req.params.id], (err, result) => {
+            if (err) {
+                res.status(501).send("Erro ao executar a qry " + err);
+                return console.error("Erro ao executar a qry de DELETE", err);
+            } else {
+                if (result.rowCount == 0) {
+                    res.status(404).json({ info: "Registro não encontrado." });
+                } else {
+                    res.status(200).json({ info: `Registro excluído. Código: ${req.params.id}` });
+                }
+            }
+            console.log(result);
+        }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//Rota para inserir um usuario
+app.post("/usuarios", (req, res) => {
+    try {
+        console.log("Alguém enviou um post com os dados:", req.body);
+        const { nome, email, altura, peso } = req.body;
+        client.query("INSERT INTO Usuarios (nome, email, altura, peso) VALUES ($1, $2, $3, $4) RETURNING * ", [nome, email, altura, peso], (err, result) => {
+            if (err) {
+                res.status(501).send("Erro ao executar a qry " + err);
+                return console.error("Erro ao executar a qry de INSERT", err);
+            }
+            const { id } = result.rows[0];
+            res.setHeader("id", `${id}`);
+            res.status(201).json(result.rows[0]);
+            console.log(result);
+        }
+        );
+    } catch (erro) {
+        console.error(erro);
+    }
+});
+
+//Rota para atualizar dados de um usuario
 app.put("/usuarios/:id", (req, res) => {
     try {
         console.log("Alguém enviou um update com os dados:", req.body);
-        const id = req.params.id
-            ;
-        const { nome, email, altura, peso } = req.body
-            ;
-        client.query
-            (
-                "UPDATE Usuarios SET nome=$1, email=$2, altura=$3, peso=$4 WHERE id =$5 ",
-                [nome, email, altura, peso, id],
-                (err, result) => {
-                    if
-                        (err) {
-                        res.status
-                            (501).send
-                            ("Erro ao executar a qry" + err);
+        const id = req.params.id;
+        const {nome, email, altura, peso } = req.body;
+        client.query("UPDATE Usuarios SET nome=$1, email=$2, altura=$3, peso=$4 WHERE id =$5 ", [nome, email, altura, peso, id],(err, result) => {
+                    if(err){
+                        res.status(501).send("Erro ao executar a qry" + err);
                         return console.error("Erro ao executar a qry de UPDATE", err);
                     } else {
                         res.setHeader("id", id);
                         res.status(202).json({ "identificador": id });
-                        console.log
-                            (result);
+                        console.log(result);
 
                     }
-
                 }
             );
     } catch (erro) {
         console.error(erro);
-
     }
 });
 
-
-
-app.get("/", (req, res) => {
-    console.log("Response ok.");
-    res.status(200).send("Ok – Servidor disponível.");
-});
-
-//ler as rotas(deve ser o ultimo comando)
+//Tornar a API ativa na porta 9082
 app.listen(config.PORT, () =>
     console.log("Servidor funcionando na porta " + config.PORT)
 );
 
-module.exports = app
+module.exports = app; 
